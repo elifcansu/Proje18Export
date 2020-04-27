e4979aaed20cd21bb598c0acfee38d0460b5be19
elifcansu.001@outlook.com <elifcansu.001@outlook.com>
2020-04-28 01:59:19 +0300

    Add project files.

---
+++ Proje18Export/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proje18Export.Models.Entity;
+
+
+namespace Proje18Export.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        MvcDBStokEntities db = new MvcDBStokEntities();
+        public ActionResult Index()
+        {
+            var degerler=db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+    }
+}
